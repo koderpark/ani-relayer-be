7f6471062646c9ab5d7e295e21aa4fb8c3d881bf
박성훈 / Koder <koder0205@gmail.com>
2025-10-01 02:11:32 +0900

    query roominfo by uuid

--- src/room/room.controller.ts
+++ src/room/room.controller.ts
@@ -21,4 +21,10 @@ export class RoomController {
   async findAll(): Promise<PublicRoom[]> {
     return this.roomService.readAllPublic();
   }
+
+  @Get(':uuid')
+  async findByUuid(@Param('uuid') uuid: string): Promise<PublicRoom> {
+    const room = await this.roomService.readByUuid(uuid, ['users', 'host']);
+    return this.roomService.publicRoom(room);
+  }
 }
